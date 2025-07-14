f191fe1067e20ec50f5d18ba51bad063d08c272e
SpiderMan0777 <spideyofficial777@gmail.com>
2025-07-14 10:49:47 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -78,3 +78,11 @@ logger = logging.getLogger(__name__)
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",     
+        port=10000,         
+        reload=False        
+    )
