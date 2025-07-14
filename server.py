93f87b8ac2e5eade46565a97cef77a72c8825603
SpiderMan0777 <spideyofficial777@gmail.com>
2025-07-14 10:59:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ async def shutdown_db_client():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "server:app",
         host="0.0.0.0",     
         port=10000,         
         reload=False        
